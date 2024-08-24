ad0d766a4a28c66755f896d98ee4ff8e7ebd5b7b
HimashRajapaksha <rajapakshahimash@gmail.com>
2024-08-24 11:48:05 +0530

    fix js

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ let hill1 = document.getElementById('hill1');
 let hill4 = document.getElementById('hill4');
 let hill5 = document.getElementById('hill5');
 
-window.addEventListener('scroll',()=>{
-    let value = window.scrollY;
+// window.addEventListener('scroll',()=>{
+//     let value = window.scrollY;
 
-    text.style.marginTop = value * 2.5 +  'px';
-    leaf.style.top = value * -1.5 +'px';
-    leaf.style.left = value * 1.5 +'px';
-    hill5.style.left = value * 1.5 +'px';
-    hill4.style.left = value * -1.5 +'px';
-    hill1.style.top = value * 1 +'px';
-});
+//     text.style.marginTop = value * 2.5 +  'px';
+//     leaf.style.top = value * -1.5 +'px';
+//     leaf.style.left = value * 1.5 +'px';
+//     hill5.style.left = value * 1.5 +'px';
+//     hill4.style.left = value * -1.5 +'px';
+//     hill1.style.top = value * 1 +'px';
+// });
